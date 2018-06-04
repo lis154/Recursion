402a9b8d60e6e791ceeeb377b02c1f3167362e51
lis154 <lis154@mail.ru>
2018-06-04 09:42:03 +0300

    commit_tower

--- src/com/company/HanTower/HanTower.java
+++ src/com/company/HanTower/HanTower.java
@@ -1,5 +1,27 @@
 package com.company.HanTower;
 
+import java.util.Scanner;
+
 public class HanTower {
 
+    public void TOH(int n, String start, String aux, String end) {
+
+        if (n == 1) {
+            System.out.println(start + " -> " + end);
+        } else {
+            TOH(n - 1, start, end, aux);
+            System.out.println(start + " -> " + end);
+            TOH(n - 1, aux, start, end);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        HanTower towersOfHanoi = new HanTower();
+
+        System.out.print("Enter number of discs: ");
+        Scanner scanner = new Scanner(System.in);
+        int discs = scanner.nextInt();
+        towersOfHanoi.TOH(discs, "A", "B", "C");
+    }
 }
